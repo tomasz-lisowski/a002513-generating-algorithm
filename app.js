28c8bc97b23a699c796129887e2c4adde3c52bb2
Tomasz Lisowski <t.p.lisowski@gmail.com>
2018-03-10 17:51:59 +0100

    Added comments

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+// first ask for the degree to be computed
 var stdin = process.openStdin();
-
 console.log("Input degree of polynomial: ")
 stdin.addListener("data", function(d) {
-    console.log(d.toString().trim());
+    var degree = d.toString().trim();
 });
+
+// find the number of permutations
+let find = function () {
+  for (i = 0; i <=degree; i++){
+    console.log(i + degree)
+  }
+}
