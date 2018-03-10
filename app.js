83117912c7884ed89d648512c4080167e4ee35e4
Tomasz Lisowski <t.p.lisowski@gmail.com>
2018-03-10 17:49:30 +0100

    Tell user what to input

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var stdin = process.openStdin();
 
+console.log("Input degree of polynomial: ")
 stdin.addListener("data", function(d) {
-  console.log("Input degree of polynomial: ")
   console.log("you entered: [" +
   d.toString().trim() + "]");
 });
