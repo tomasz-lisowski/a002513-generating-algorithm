7d74d125897b9301898f42e705dc0612eac5f15d
Tomasz Lisowski <t.p.lisowski@gmail.com>
2018-03-10 18:07:42 +0100

    Return all permutations of a (user-specified degree) polynomial

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 // first ask for the degree to be computed
+console.log("/-----------------")
 var stdin = process.openStdin();
 console.log("Input degree of polynomial: ")
 stdin.addListener("data", function(d) {
-    var degree = d.toString().trim();
+  let polynomialDegree = d.toString().trim();
+  console.log("============");
+  find(polynomialDegree);
 });
 
 // find the number of permutations
-let find = function () {
-  for (i = 0; i <=degree; i++){
-    console.log(i + degree)
+const find = function (polynomialDegree) {
+  var combinationsArray = [];
+  var imaginaryArray = [];
+  var realArray = [];
+  // create imaginary and real root sets
+  for(var i = 0;i <= polynomialDegree;i++){
+    // start imagiary set with '0'
+    if(i === 0){
+      imaginaryArray.push(i);
+    }
+    // add multiples of '2' to imaginary set
+    else if (i % 2 === 0) {
+      imaginaryArray.push(i);
+    }
+    // add every integer >= to polynomial degree to real set
+    realArray.push(i);
   }
-}
+  // crete combinatoric set relating each real with every imaginary number
+  realArray.forEach(function(elementR) {
+    imaginaryArray.forEach(function(elementI) {
+      combinationsArray.push([elementR,elementI]);
+    });
+  });
+  combinationsArray.shift()
+  console.log(combinationsArray);
+  console.log("============");
+  console.log(combinationsArray.length);
+  console.log("\-----------------")
+};
