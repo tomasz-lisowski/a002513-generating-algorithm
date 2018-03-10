07b11c122291fa4af3f5206155ae4fff08a47796
Tomasz Lisowski <t.p.lisowski@gmail.com>
2018-03-10 17:50:13 +0100

    Check input

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ var stdin = process.openStdin();
 
 console.log("Input degree of polynomial: ")
 stdin.addListener("data", function(d) {
-  console.log("you entered: [" +
-  d.toString().trim() + "]");
+    console.log(d.toString().trim());
 });
