4199883fd16d795cc4ab16123e17493cc4e9eb49
Tomasz Lisowski <t.p.lisowski@gmail.com>
2018-07-01 17:50:29 +0200

    rev 2.1 will find combinations of real roots
    rev 2.1 aims to take a polynomial degree and compute combinations of real roots taking into account repeated and unique real roots and combinations of both legally satisfying constrains for the specified degree

--- app.js
+++ app.js
@@ -1,89 +1,96 @@
 // 1, 1, 3, 4, 9, 12, 23, 31, 54, 73
 // first ask for the degree to be computed
+console.log("\n" + "Input degree of polynomial: ");
 console.log("═══════════════════════════");
 var stdin = process.openStdin();
-console.log("Input degree of polynomial: ");
 stdin.addListener("data", function(d) {
 	polynomialDegree = d.toString().trim();
 	compute(polynomialDegree);
 });
 
-var combinationsArray = [];
-var imaginaryArray = [];
-var realArray = [];
+var integerSet = [];
 
-// check if sum of real and imaginary roots is not bigger than polynomial degree
-const checkRootSum = function (elementR, elementI) {
-	if ((elementR + elementI) <= polynomialDegree) {
-		return true;
-	}
-};
+var uniqueRealArray = [];
+var repeatedRealArray = [];
+
+var masterSet = [];
 
 // is the degree odd or even?
 const checkDegreeParity = function () {
-	if (polynomialDegree % 2 !== 0) {
-		return 'odd';
+	if (isNaN(polynomialDegree) === false) {
+		if (polynomialDegree % 2 !== 0) {
+			return 'odd';
+		}
+		else if (polynomialDegree % 2 === 0) {
+			return 'even';
+		}
 	}
 	else {
-		return 'even';
 	}
-};
-
-// compute for odd degrees
-const computeForOdd = function (elementR, elementI) {
-	// there will always be 1 real root
-	// if there are no real roots its invalid (must intersect through x at y=0)
-	if (elementR !== 0) {
-		combinationsArray.push([elementR, elementI]);
-	}
-};
-
-//compute for even degrees
-const computeForEven = function (elementR, elementI) {
-	combinationsArray.push([elementR, elementI]);
 };
 
 // create collection of integers to be used in current computation (appropraite for current degree)
 const createIntegerSet = function () {
 	for (let i = 0; i <= polynomialDegree; i++){
 		// add every integer not bigger than polynomial degree to real set
-		realArray.push(i);
-		// add multiples of '2' not bigger than polynomial degree to imaginary set
-		if (i % 2 === 0) {
-			imaginaryArray.push(i);
-		}
+		integerSet.push(i);
 	}
 }
+
+// run loops for all integers in integer sequence
+// these loops create arrays axlusive to only one type of root combination
+//(no higher numerals will be used in the following computation)
+const initiateRealArrays = function (parity) {
+	if (parity === 'odd') {
+		integerSet.forEach(function(Ur) {
+			if (Ur !== 0) {
+				uniqueRealArray.push([Ur, 0, 0, 0]);
+			}
+		});
+		integerSet.forEach(function(Rr) {
+			if (Rr !== 0 && Rr % 2 === 0) {
+				repeatedRealArray.push([0, Rr, 0, 0]);
+			}
+		});
+	}
+	else if (parity === 'even') {
+		integerSet.forEach(function(Ur) {
+			if (Ur !== 0) {
+				uniqueRealArray.push([Ur, 0, 0, 0]);
+			}
+		});
+		integerSet.forEach(function(Rr) {
+			if (Rr !== 0 && Rr % 2 === 0) {
+				repeatedRealArray.push([0, Rr, 0, 0]);
+			}
+		});
+	}
+};
 
 // find the answer
 const compute = function (polynomialDegree) {
 	createIntegerSet();
-	// relate each element of the real set to imaginary set
-	realArray.forEach(function(elementR) {
+	initiateRealArrays(checkDegreeParity(polynomialDegree));
 
-		imaginaryArray.forEach(function(elementI) {
-			// make sure that no element contains no roots i.e. elements like [0, 0]
-			if (!(elementR === 0 && elementI === 0)) {
-				// sum of roots cant be bigger than polynomial degree
-				if (checkRootSum(elementR, elementI) === true) {
-					// run for odd degree
-					if (checkDegreeParity() === 'odd') {
-						computeForOdd(elementR, elementI);
-					}
-					// run for even degree
-					else {
-						computeForEven(elementR, elementI);
-					}
-				}
+	// for every element in Ur check if a repeated root could appear
+	for (var i = 0; i < uniqueRealArray.length; i++) {
+		for (var j = 2; j <= polynomialDegree; j = j + 2) {
+			if ((uniqueRealArray[i][0] + j) <= polynomialDegree) {
+				masterSet.push([uniqueRealArray[i][0], j, 0, 0]);
 			}
-		});
-	});
+		}
+	}
+
+	// formulate the answer
+	masterSet.push(...uniqueRealArray);
+	masterSet.push(...repeatedRealArray);
 	// print out the answer
-	console.log(combinationsArray);
-	console.log("\n" + combinationsArray.length);
+	console.log(masterSet);
+	console.log(masterSet.length);
 	console.log("═══════════════════════════");
 	// clear arrays after computation
-	combinationsArray = [];
-	imaginaryArray = [];
-	realArray = [];
+	integerSet = [];
+	uniqueRealArray = [];
+	repeatedRealArray = [];
+	masterSet = [];
 };
