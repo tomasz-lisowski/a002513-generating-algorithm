304a7933d68facf94f2e974c5e5f005f12e8db40
Tomasz Lisowski <t.p.lisowski@gmail.com>
2018-07-02 14:30:48 +0200

    Fix faulty logic
    previous check was looking for any number that was not 0 and now looks for numbers bigger than 0 because there can be no negative root combination

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const compute = function (polynomialDegree) {
 	// try if there can either be a repeated real root or two imaginary roots
 	for (let i = 2; i < polynomialDegree; i = i + 2) {
 		// check if first element will not equal 0 if i is subtracted from it
-		if (!(masterArray[0][0] - i <= 0)) {
+		if (masterArray[0][0] - i > 0) {
 			// if first element will not have to equal zero
 			// when i is pushed at either 2nd (Rr) or 3rd (Ui) position
 			// this is possible because for both Rr and Ui
@@ -60,7 +60,7 @@ const compute = function (polynomialDegree) {
 	// try if there can be a multiplicity of imaginary roots
 	for (let i = 4; i < polynomialDegree; i = i + 4) {
 		// check if first element will not equal 0 if i is subtracted from it
-		if (!(masterArray[0][0] - i <= 0)) {
+		if (masterArray[0][0] - i > 0) {
 			cArray.push([masterArray[0][0] - i, 0, 0, i])
 		}
 	}
@@ -69,12 +69,12 @@ const compute = function (polynomialDegree) {
 		// try if a 3rd element can fit while not letting 1st or 2nd to equal 0
 		for (let j = 2; j < polynomialDegree; j = j + 2) {
 			// check if first element will not equal 0 if i is subtracted from it
-			if (!(aArray[i][0] - j <= 0)) {
 				dArray.push(aArray[i][0] - j, aArray[i][1], j, 0)
+			if (aArray[i][0] - j > 0) {
 			}
 			// check if second element will not equal 0 if i is subtracted from it
-			if (!(aArray[i][1] - j <= 0)) {
 				eArray.push(aArray[i][0], aArray[i][1] - j, j, 0)
+			if (aArray[i][1] - j > 0) {
 			}
 		}
 	}
@@ -82,16 +82,16 @@ const compute = function (polynomialDegree) {
 	for (let i = 0; i < dArray.length; i++) {
 		for (let j = 4; j < polynomialDegree; j = j + 4) {
 			// check if first element will not equal 0 if i is subtracted from it
-			if (!(dArray[i][0] - j <= 0)) {
 				fArray.push(dArray[i][0] - j, dArray[i][1], dArray[i][2], j)
+			if (dArray[i][0] - j > 0) {
 			}
 			// check if second element will not equal 0 if i is subtracted from it
-			if (!(dArray[i][1] - j <= 0)) {
 				gArray.push(dArray[i][0], dArray[i][1] - j, dArray[i][2], j)
+			if (dArray[i][1] - j > 0) {
 			}
 			// check if third element will not equal 0 if i is subtracted from it
-			if (!(dArray[i][2] - j <= 0)) {
 				hArray.push(dArray[i][0], dArray[i][1], dArray[i][2] - j, j)
+			if (dArray[i][2] - j > 0) {
 			}
 		}
 	}
