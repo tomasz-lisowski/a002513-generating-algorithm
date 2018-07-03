070582b2b17df8da73f6fab4668b5a7638056dcf
Tomasz Lisowski <t.p.lisowski@gmail.com>
2018-07-03 02:59:57 +0200

    Fix, simplify logic and clean up comments

--- app.js
+++ app.js
@@ -42,44 +42,48 @@ const checkDegreeParity = function () {
 	}
 	else {
 		throw "Parameter is not a number!";
-		return;
 	}
 };
 
 // find the answer
 const compute = function (polynomialDegree) {
+	const parity = checkDegreeParity(polynomialDegree);
+
 	// create one sort of root combination (Ur)
 	masterArray.push([polynomialDegree, 0, 0, 0]);
-	// create one sort of root combination (Rr, Ui)
-	for (let i = 0; i < polynomialDegree; i = i + 2) {
-		if (polynomialDegree % 2 === 0) {
-			masterArray.push([0, polynomialDegree, 0, 0]);
-			masterArray.push([0, 0, polynomialDegree, 0]);
+	if (polynomialDegree >= 2) {
+		// create one sort of root combination (Rr)
+		masterArray.push([0, polynomialDegree, 0, 0]);
+	}
+	if (polynomialDegree % 2 === 0) {
+		// create one sort of root combination (Ui)
+		masterArray.push([0, 0, polynomialDegree, 0]);
+	}
+	if (polynomialDegree % 4 === 0) {
+		// create one sort of root combination (Ri)
+		masterArray.push([0, 0, 0, polynomialDegree]);
+	}
+
+	// try if there can either be a repeated real root
+	for (let i = 1; i < polynomialDegree; i = i + 1) {
+		// check if Ur + Rr = polynomialDegree
+		if (masterArray[0][0] + i === polynomialDegree) {
+			aArray.push([masterArray[0][0] - i, i, 0, 0]);
 		}
 	}
-	// create one sort of root combination (Ri)
-	for (let i = 0; i < polynomialDegree; i = i + 4) {
-		if (polynomialDegree % 4 === 0) {
-			masterArray.push([0, 0, 0, polynomialDegree]);
-		}
-	}
-	// try if there can either be a repeated real root or two imaginary roots
+	// try if there can be unique imaginary roots with conjugate pair
 	for (let i = 2; i < polynomialDegree; i = i + 2) {
 		// check if first element will not equal 0 if i is subtracted from it
-		if (masterArray[0][0] - i > 0) {
-			// if first element will not have to equal zero
-			// when i is pushed at either 2nd (Rr) or 3rd (Ui) position
-			// this is possible because for both Rr and Ui
-			// requirements that i must be multiple of 2 is met
-			aArray.push([masterArray[0][0] - i, i, 0, 0]);
+		if (masterArray[0][0] + i === polynomialDegree) {
 			bArray.push([masterArray[0][0] - i, 0, i, 0]);
 		}
 	}
+
 	// try if there can be a multiplicity of imaginary roots
 	for (let i = 4; i < polynomialDegree; i = i + 4) {
 		// check if first element will not equal 0 if i is subtracted from it
 		if (masterArray[0][0] - i > 0) {
-			cArray.push([masterArray[0][0] - i, 0, 0, i])
+			cArray.push([masterArray[0][0] - i, 0, 0, i]);
 		}
 	}
 	// make sure to run loop for every element in array A
@@ -88,11 +92,11 @@ const compute = function (polynomialDegree) {
 		for (let j = 2; j < polynomialDegree; j = j + 2) {
 			// check if first element will not equal 0 if i is subtracted from it
 			if (aArray[i][0] - j > 0) {
-				dArray.push([aArray[i][0] - j, aArray[i][1], j, 0])
+				dArray.push([aArray[i][0] - j, aArray[i][1], j, 0]);
 			}
 			// check if second element will not equal 0 if i is subtracted from it
 			if (aArray[i][1] - j > 0) {
-				eArray.push([aArray[i][0], aArray[i][1] - j, j, 0])
+				eArray.push([aArray[i][0], aArray[i][1] - j, j, 0]);
 			}
 		}
 	}
@@ -101,15 +105,15 @@ const compute = function (polynomialDegree) {
 		for (let j = 4; j < polynomialDegree; j = j + 4) {
 			// check if first element will not equal 0 if i is subtracted from it
 			if (dArray[i][0] - j > 0) {
-				fArray.push([dArray[i][0] - j, dArray[i][1], dArray[i][2], j])
+				fArray.push([dArray[i][0] - j, dArray[i][1], dArray[i][2], j]);
 			}
 			// check if second element will not equal 0 if i is subtracted from it
 			if (dArray[i][1] - j > 0) {
-				gArray.push([dArray[i][0], dArray[i][1] - j, dArray[i][2], j])
+				gArray.push([dArray[i][0], dArray[i][1] - j, dArray[i][2], j]);
 			}
 			// check if third element will not equal 0 if i is subtracted from it
 			if (dArray[i][2] - j > 0) {
-				hArray.push([dArray[i][0], dArray[i][1], dArray[i][2] - j, j])
+				hArray.push([dArray[i][0], dArray[i][1], dArray[i][2] - j, j]);
 			}
 		}
 	}
@@ -129,6 +133,7 @@ const compute = function (polynomialDegree) {
 
 
 	// print out the answer
+	//console.log(aArray);
 	console.log(masterArray);
 	console.log(masterArray.length);
 	console.log("═══════════════════════════");
